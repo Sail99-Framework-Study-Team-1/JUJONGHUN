4e1ea454eb2bba2d58b0164af0de0399f8daf42a
wn1331 <wn1331@gmail.com>
2024-07-29 09:54:18 +0900

    chore : BaseEntity setting

--- Board/src/main/java/study/board/domain/entity/BaseEntity.java
+++ Board/src/main/java/study/board/domain/entity/BaseEntity.java
@@ -1,5 +1,21 @@
 package study.board.domain.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Persistable<String> {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
 }
