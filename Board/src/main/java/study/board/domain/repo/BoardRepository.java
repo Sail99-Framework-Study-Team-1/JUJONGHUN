5c9953bbd61906c24f41515a53b5ed43b6a27365
wn1331 <wn1331@gmail.com>
2024-07-29 09:22:06 +0900

    chore : postgresql setting

--- Board/src/main/java/study/board/domain/repo/BoardRepository.java
+++ Board/src/main/java/study/board/domain/repo/BoardRepository.java
@@ -3,6 +3,6 @@ package study.board.domain.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.board.domain.entity.Board;
 
-public interface BoardRepository extends JpaRepository<Long, Board> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
--- Board/src/main/java/study/board/presentation/BoardController.java
+++ Board/src/main/java/study/board/presentation/BoardController.java
@@ -1,10 +1,12 @@
 package study.board.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.board.global.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +15,8 @@ public class BoardController {
 
 
     @GetMapping("/board")
-    public ResponseEntity example(){
-        return
+    public ResponseEntity<ApiResponse<String>> example(){
+        return ApiResponse.createResponse(true,"안녕","resquest successful", HttpStatus.CREATED);
     }
 
 
