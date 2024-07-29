441c6af6bc1fd4c53925427c4c04ad57ce3b2c3f
wn1331 <wn1331@gmail.com>
2024-07-29 15:32:53 +0900

    chore : ModelMapper 의존성 제거

--- Board/src/main/java/study/board/application/BoardService.java
+++ Board/src/main/java/study/board/application/BoardService.java
@@ -2,13 +2,10 @@ package study.board.application;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.board.domain.entity.Board;
 import study.board.domain.repo.BoardRepository;
-import study.board.global.exception.BoardException;
-import study.board.global.exception.ErrorCode;
 import study.board.presentation.dto.BoardRequestDto;
 import study.board.presentation.dto.BoardResponseDto;
 
@@ -16,8 +13,6 @@ import study.board.presentation.dto.BoardResponseDto;
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
-    private final ModelMapper mapper;
-
 
     @Transactional
     public Long publishBoard(BoardRequestDto requestDto){
