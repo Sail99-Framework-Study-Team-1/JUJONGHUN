fb00d15d3b5947c464fc49244f2b768df2495e5d
wn1331 <wn1331@gmail.com>
2024-07-29 09:11:32 +0900

    chore : ApiResponse<T> 기초세팅

--- Board/src/main/java/study/board/global/ApiResponse.java
+++ Board/src/main/java/study/board/global/ApiResponse.java
@@ -1,5 +1,24 @@
 package study.board.global;
 
-public class ApiResponse {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+@Getter
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private String message;
+
+    public ApiResponse(boolean success, T data, String message) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+
+    public static <T> ResponseEntity<ApiResponse<T>> createResponse(boolean success, T data, String message, HttpStatus status) {
+        return new ResponseEntity<>(new ApiResponse<>(success, data, message), status);
+    }
 }
--- Board/src/main/java/study/board/presentation/BoardController.java
+++ Board/src/main/java/study/board/presentation/BoardController.java
@@ -1,6 +1,8 @@
 package study.board.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping
 public class BoardController {
+
+
+    @GetMapping("/board")
+    public ResponseEntity example(){
+        return
+    }
 
 
 
